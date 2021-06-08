45d57204de794c24648396594d34658b9f8bb696
80Hefner <luisenesousa@gmail.com>
2021-06-08 19:25:54 +0100

    trabalho grupo

---
+++ TGrupo/fasesVacinacao.pl
@@ -0,0 +1,27 @@
+% Trabalho Prático SRCR 20/21.
+
+% ----------------------------------------------------------------------------------------------
+% Definir as 2 fases de vacinação.
+% ----------------------------------------------------------------------------------------------
+
+% ----------------------------------------------------------------------------------------------
+% 1ª fase de vacinação, estende-se desde 01-01-2021 até 31-03-2021.
+% Em relação a idades, na primeira fase de vacinação serão vacinadas as pessoas com idade superior a 80 anos.
+% Em relação a profissões, na primeira fase de vacinação serão vacinados profissionais de saúde e profissionais de setores de segurança nacional.
+% Em relação a doenças crónicas, na primeira fase de vacinação serão vacinadas pessoas com problemas cardíacos e doenças respiratórias crónicas.
+% ----------------------------------------------------------------------------------------------
+
+%primeira_fase_Vacinacao: Idade > 80; 'Médico'; 'Enfermeiro'; 'Militar'; 'Doença Pulmonar Obstrutiva'; 'Doenças Cardiovasculares'; 'Hipertensão'.
+
+% ----------------------------------------------------------------------------------------------
+% 2ª fase de vacinação, estende-se desde 01-04-2021 até 31-07-2021.
+% Em relação a idades, na segunda fase de vacinação serão vacinadas as pessoas com idade superior a 50 anos.
+% Em relação a profissões, na segunda fase de vacinação serão vacinados profissionais da área da educação e estudantes.
+% Em relação a doenças crónicas, na segunda fase de vacinação serão vacinadas pessoas com diabetes, obesidade, asma e cancro.
+% ----------------------------------------------------------------------------------------------
+
+%segunda_fase_Vacinacao: Idade > 50; 'Professor'; 'Estudante'; 'Diabetes'; 'Obesidade'; 'Asma'; 'Cancro'.
+
+% ----------------------------------------------------------------------------------------------
+% Numa possível 3ª fase, será vacinada toda a restante população elegível, que poderá ser igualmente priorizada.
+% ----------------------------------------------------------------------------------------------
